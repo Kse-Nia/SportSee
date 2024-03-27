3660fa705de2f7808bce02e5c066864629bc2b37
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-27 20:17:12 +0100

    progression routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Components
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
