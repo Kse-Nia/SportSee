f438917032b4b2e6234337ed97d73678fab3a2ca
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2025-11-04 18:11:53 +0100

    Add Home component and update main.tsx with BrowserRouter

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,7 +1,8 @@
+import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchData } from "./utils/fetch"; // Fetching function
-import mockupData from "./data/mockup.json"; // JSON data instead of API
-import Layout from "./components/Layout"; // Layout, page organization
+
+import Layout from "./components/Layout"; // Import Navbars
+
 import UserWelcome from "./components/UserWelcome"; // Welcome component
 import HealthMetrics from "./components/HealthMetrics"; // Health metrics component
 import DailyActivity from "./components/DailyActivity"; // Daily activity component
@@ -11,140 +12,22 @@ import Duration from "./components/charts/Duration"; // Duration component
 
 const userUrl = import.meta.env.VITE_USER_URL;
 
-import { checkScore } from "./utils/formatData"; // check score function
-
-// Activities Icons
-import Calories from "./assets/icons/calories.svg";
-import Protein from "./assets/icons/protein.svg";
-import Carbs from "./assets/icons/carbs.svg";
-import Fat from "./assets/icons/fat.svg";
-
-interface UserData {
-  userInfos: {
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
-  };
-  todayScore: number;
-  keyData: {
-    calorieCount: number;
-    proteinCount: number;
-    carbohydrateCount: number;
-    lipidCount: number;
-  };
-}
-interface UserActivity {
-  id: number;
-  sessions: Session[];
-}
-
-interface Session {
-  day: string;
-  sessionsWeight: number;
-  calories: number;
-}
-
-interface UserPerformance {
-  id: number;
-  kind: string;
-  data: {
-    value: number;
-    kind: string;
-  };
-}
-
-export default function Home() {
-  const [userData, setUserData] = useState<UserData | null>(null); // User data
-  const [userActivity, setUserActivity] = useState<UserActivity | null>(null); // User activity
-  const [userSessions, setUserSessions] = useState<Session[] | null>(null); // User sessions
-  const [userPerformance, setUserPerformance] =
-    useState<UserPerformance | null>(null); // User performance
-
-  const findScore = checkScore(userData); // Get the right score
-
-  // Hook to fetch data API
-  useEffect(() => {
-    const fetchAllData = async () => {
-      try {
-        const fetchUserData = await fetchData(`${userUrl}/12`, mockupData);
-        const fetchUserActivity = await fetchData(
-          `${userUrl}/12/activity`,
-          mockupData
-        );
-        const fetchUserSessions = await fetchData(
-          `${userUrl}/12/average-sessions`,
-          mockupData
-        );
-        const fetchUserPerformance = await fetchData(
-          `${userUrl}/12/performance`,
-          mockupData
-        );
-
-        if (fetchUserData) setUserData(fetchUserData.data);
-        if (fetchUserActivity) setUserActivity(fetchUserActivity.data);
-        if (fetchUserSessions) setUserSessions(fetchUserSessions.data.sessions);
-        if (fetchUserPerformance) setUserPerformance(fetchUserPerformance.data);
-      } catch (err) {
-        console.log("Error, impossible de récupérer la data : ", err);
-      }
-    };
-    fetchAllData();
-  }, []);
-
-  // Metrics data for the HealthMetrics component
-  const metrics = userData
-    ? [
-        {
-          icon: Calories,
-          count: userData.keyData.calorieCount,
-          type: "Calories",
-        },
-        {
-          icon: Protein,
-          count: userData.keyData.proteinCount,
-          type: "Protéines",
-        },
-        {
-          icon: Carbs,
-          count: userData.keyData.carbohydrateCount,
-          type: "Glucides",
-        },
-        {
-          icon: Fat,
-          count: userData.keyData.lipidCount,
-          type: "Lipides",
-        },
-      ]
-    : [];
+export default function App() {
+  const userData = {};
+  const userActivity = {};
+  const userSessions = {};
 
   return (
-    <main className="dashboard">
-      <div className="dashboard__welcome">
-        {userData && <UserWelcome userInfos={userData.userInfos} />}
-      </div>
-      <div className="dashboard__content">
-        <div className="dashboard__activity">
-          {userActivity && (
-            <>
-              <div className="activity__daily">
-                <DailyActivity
-                  dailyActivity={userActivity.sessions}
-                  dailyActivityWeight={userActivity.sessionsWeight}
-                />
-              </div>
-              <div className="activity__graphs">
-                <Duration sessionDuration={userSessions} />
-                <RadarGraphe performances={userPerformance} />
-                <Score score={findScore} />
-              </div>
-            </>
-          )}
-        </div>
-        <div className="dashboard__health-metrics">
-          {userData && <HealthMetrics metrics={metrics} />}
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={<Home />}
+          userData={userData}
+          userActivity={userActivity}
+          userSessions={userSessions}
+        />
+      </Route>
+    </Routes>
   );
 }
--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
+import { BrowserRouter } from "react-router";
 import "./style/main.scss";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
