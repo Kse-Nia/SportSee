bd3a40550e1acfdcfd737cd103ebd5a09d4e9c72
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2025-11-04 18:09:22 +0100

    Refactor Layout component to use Outlet for nav and side bars

--- my-react-app/src/components/Layout.tsx
+++ my-react-app/src/components/Layout.tsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router"; //
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import React from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
-
+/* 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
@@ -14,6 +16,25 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <Sidebar />
         </div>
         <div className="main">{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
+ */
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="main">
+          <Outlet />
+        </div>
       </div>
     </>
   );
