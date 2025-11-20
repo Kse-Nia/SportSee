45f32b310bf148bedb00bff237a10f3555a036d0
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2025-11-20 20:37:42 +0100

    fix fetch function, mockup is working

--- my-react-app/src/utils/fetch.ts
+++ my-react-app/src/utils/fetch.ts
@@ -1,6 +1,6 @@
 import mockupData from "../data/mockup.json";
 
-export async function fetchData(url: string) {
+/* export async function fetchData(url: string) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -16,6 +16,44 @@ export async function fetchData(url: string) {
       return mockupData;
     } catch (error) {
       console.error("Échec du chargement des données :", error);
+      return null;
+    }
+  }
+} */
+
+export async function fetchData(url: string) {
+  try {
+    // Fetching from backend
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Backend is running", data);
+    return data;
+  } catch (err) {
+    console.warn(err);
+    // Running JSON if  backend is unavailable
+    try {
+      console.log("Local JSON data"); // check data 
+
+      const urlParts = url.split("/"); // split URL to get user ID
+      const userIdIndex = urlParts.indexOf("user") + 1;
+      const userId = parseInt(urlParts[userIdIndex]);
+
+      // Searching user
+      const user = Array.isArray(mockupData)
+        ? mockupData.find((u: any) => u.id === userId)
+        : mockupData;
+
+      if (user) {
+        return { data: user };
+      } else {
+        console.error("Cannot find user");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error, cannot get data :", error);
       return null;
     }
   }
