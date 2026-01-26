abb3da9db498d05b6b977af5f1be6d6a2b6c7d8e
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-01-26 19:59:35 +0100

    Refactor Sidebar component: update structure, replace divs with semantic elements

--- my-react-app/src/components/bars/Sidebar.tsx
+++ my-react-app/src/components/bars/Sidebar.tsx
@@ -1,25 +1,35 @@
 import React from "react";
-
 export default function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="sidebar__activity">
-        <div className="sidebar__activity-icon">
-          <img src="/public/yoga.svg" alt="Yoga icon" />
-        </div>
-        <div className="sidebar__activity-icon">
-          <img src="/public/swim.svg" alt="Swim icon" />
-        </div>
-        <div className="sidebar__activity-icon">
-          <img src="/public/bike.svg" alt="Bike icon" />
-        </div>
-        <div className="sidebar__activity-icon">
-          <img src="/public/dumbbell.svg" alt="Dumbbell icon" />
-        </div>
-      </div>
+    <aside className="sidebar">
+      <nav className="sidebar__nav">
+        <ul className="sidebar__list">
+          <li className="sidebar__item">
+            <a href="#" className="sidebar__link">
+              <img src="/yoga.svg" alt="Yoga" />
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="#" className="sidebar__link">
+              <img src="/swim.svg" alt="Natation" />
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="#" className="sidebar__link">
+              <img src="/bike.svg" alt="Cyclisme" />
+            </a>
+          </li>
+          <li className="sidebar__item">
+            <a href="#" className="sidebar__link">
+              <img src="/dumbbell.svg" alt="Musculation" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      
       <div className="sidebar__copyright">
-        <p>Copiryght, SportSee 2020</p>
+        <p>Copyright, SportSee 2020</p>
       </div>
-    </div>
+    </aside>
   );
 }
