569f1f40cdeaf919399ff33a3fe3d7ef0efe34bc
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-01-31 10:06:36 +0100

    Refactor HealthMetrics component + update and fix UI

--- my-react-app/src/components/charts/HealthMetrics.tsx
+++ my-react-app/src/components/charts/HealthMetrics.tsx
@@ -5,23 +5,42 @@ interface Metric {
   count: number;
   type: string;
 }
+
 interface HealthMetricsProps {
   metrics: Metric[];
 }
 
 const HealthMetrics: React.FC<HealthMetricsProps> = ({ metrics }) => {
+  // Fonction helper pour générer le nom du modifier
+  const getModifier = (type: string) => {
+    switch (type) {
+      case "Calories":
+        return "calories";
+      case "Protéines":
+        return "protein";
+      case "Glucides":
+        return "carbs";
+      case "Lipides":
+        return "fat";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div className="healthMetrics">
-      {metrics.map((metric, index) => (
-        <div className="healthMetrics_box" key={index}>
-          <div className="healthMetrics-icon">
-            <img src={metric.icon} alt={metric.type} width={60} height={60} />
+      {metrics.map((metric) => (
+        <div className="healthMetrics__card" key={metric.type}>
+          <div
+            className={`healthMetrics__icon healthMetrics__icon--${getModifier(
+              metric.type
+            )}`}
+          >
+            <img src={metric.icon} alt={metric.type} width={32} height={32} />
           </div>
-          <div className="healthMetrics_container">
-            <div className="healthMetrics_container-data">
-              <p className="healthMetrics_container-data-nbr">{metric.count}</p>
-              <p className="healthMetrics_container-data-type">{metric.type}</p>
-            </div>
+          <div className="healthMetrics__info">
+            <p className="healthMetrics__value">{metric.count}</p>
+            <p className="healthMetrics__label">{metric.type}</p>
           </div>
         </div>
       ))}
