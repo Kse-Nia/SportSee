8737922cf6b6302de53f90037c379d44cb13051f
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-02-22 08:29:32 +0100

    Add TypeScript interfaces for user data, activity, performance, and health metrics

--- my-react-app/src/utils/types.ts
+++ my-react-app/src/utils/types.ts
@@ -1,3 +1,40 @@
+// User
+export interface UserData {
+  userInfos: {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+  };
+  todayScore: number;
+  keyData: {
+    calorieCount: number;
+    proteinCount: number;
+    carbohydrateCount: number;
+    lipidCount: number;
+  };
+}
+export interface UserActivity {
+  id: number;
+  sessions: Session[];
+}
+
+export interface Session {
+  day: string;
+  sessionsWeight: number;
+  calories: number;
+}
+
+export interface UserPerformance {
+  id: number;
+  kind: string;
+  data: {
+    value: number;
+    kind: string;
+  };
+}
+
+
 // User main data
 export interface ActivityProps {
   dailyActivity: {
@@ -18,7 +55,6 @@ export interface ScoreData {
   score: number;
   todayScore?: number;
 }
-
 
 /* export interface SessionDuration {
   day: number;
@@ -41,4 +77,31 @@ export interface SessionDuration {
     day: string;
     sessionLength: number;
   }[];
+}
+
+export interface DurationProps {
+  sessionDuration: {
+    day: string;
+    sessionLength: number;
+  }[];
+}
+// Health metrics data
+export interface Metric {
+  icon: string;
+  count: number;
+  type: string;
+}
+export interface HealthMetricsProps {
+  metrics: Metric[];
+}
+
+// Daily activity data
+export interface ActivitySession {
+  day: string;
+  kilogram: number;
+  calories: number;
+}
+
+export interface ActivityProps {
+  dailyActivity: ActivitySession[];
 }
